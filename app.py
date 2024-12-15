1f4481a349a5e3039dd99007e0938a316f18fb25
Maria Zharakhovich <126415652+mdzharakhovich@users.noreply.github.com>
2024-12-15 14:56:27 +0300

    upd

--- app.py
+++ app.py
@@ -8,25 +8,28 @@ st.title("Прогнозирование заработка")
 st.write("Это приложение предсказывает, превысит ли ваш средний заработок порог $50k.")
 
 # Загрузка модели
-with open('best_model.pkl', 'rb') as file:
-    model = pickle.load(file)
+with open('best_model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 # Ввод данных пользователем
 st.sidebar.header("Введите данные")
-age = st.sidebar.slider("Возраст", 17, 90, 30)
-education_num = st.sidebar.slider("Образование (education-num)", 1, 16, 10)
-hours_per_week = st.sidebar.slider("Часов в неделю", 1, 99, 40)
-capital_gain = st.sidebar.number_input("Капитальный доход", 0, 99999, 0)
-capital_loss = st.sidebar.number_input("Капитальный убыток", 0, 4356, 0)
+age = st.sidebar.slider("Возраст", 18, 100, 30)
+education_num = st.sidebar.slider("Образование (число)", 1, 16, 10)
+capital_gain = st.sidebar.slider("Капитальный прирост", 0, 100000, 0)
+capital_loss = st.sidebar.slider("Капитальный убыток", 0, 10000, 0)
+hours_per_week = st.sidebar.slider("Часов в неделю", 1, 100, 40)
 
-# Преобразование данных в DataFrame
-data = pd.DataFrame({
+# Создание DataFrame из введенных данных
+input_data = pd.DataFrame({
     'age': [age],
     'education-num': [education_num],
-    'hours-per-week': [hours_per_week],
     'capital-gain': [capital_gain],
-    'capital-loss': [capital_loss]
+    'capital-loss': [capital_loss],
+    'hours-per-week': [hours_per_week]
 })
+
+# Прогнозирование
+prediction = model.predict(input_data)
 
 # Кнопка для предсказания
 if st.button("Предсказать"):
@@ -35,11 +38,3 @@ if st.button("Предсказать"):
         st.success("Ваш заработок превысит $50k!")
     else:
         st.error("Ваш заработок НЕ превысит $50k.")
-
-
-# сюда добавить лучшую модель 
-# best_model = 
-
-#import pickle
-#with open('best_model.pkl', 'wb') as file:
-  #  pickle.dump(best_model, file)
